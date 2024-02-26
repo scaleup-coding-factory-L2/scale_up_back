4047c3ace07e3544bf8de46a198595b2b2e9c530
Turbotard <turbotard9@gmail.com>
2024-02-26 14:21:58 +0100

    first test to add with a body

--- src/controllers/HelloController.ts
+++ src/controllers/HelloController.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express'
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 export const getHello = (req: Request, res: Response) => {
     res.status(200).json({ message: 'Hello World!' })
+}
+
+export const getName = async (req: Request, res: Response) => {
+    const adduser  = await prisma.test.create({
+        data: {
+            name: req.body.name,
+        },
+    })
 }
