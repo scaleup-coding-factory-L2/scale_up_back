8dccb6437c06f1d51a5aef0cb9f581c482c71d39
mathys <mathyscesaire@gmail.com>
2024-02-29 15:28:10 +0100

    feat(BACKLOG-COMMUN-2): add keycloak-connect

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import helloRoutes from './routes/helloRoutes'
 import morgan from 'morgan'
+import Keycloak from 'keycloak-connect'
+import { PrismaClient } from '@prisma/client'
+import { createUserIfNotExistsMiddleware } from './middlewares/createUserIfNotExistsMiddleware'
+
+const kcConfig = {
+    clientId: process.env.KC_CLIENT_ID,
+    bearerOnly: true,
+    serverUrl: process.env.KC_URL,
+    'ssl-required': 'external',
+    secret: process.env.KC_SECRET,
+    realm: process.env.KC_REALM,
+    'auth-server-url': process.env.KC_URL,
+    'confidential-port': 0,
+    resource: process.env.KC_CLIENT_ID
+}
+
+export const keycloak = new Keycloak({}, kcConfig)
+
+export const prisma = new PrismaClient()
 
 const app = express()
 
@@ -10,7 +31,9 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(keycloak.middleware())
+app.use(createUserIfNotExistsMiddleware)
 
-app.use('/api', helloRoutes)
+app.use('/api', keycloak.protect(), helloRoutes)
 
 export { app }
---
+++ src/middlewares/createUserIfNotExistsMiddleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express'
+import { prisma } from '../index'
+export const createUserIfNotExistsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (!req.kauth.grant) {
+            next()
+            return
+        }
+        const userUUID = req.kauth.grant.access_token.content.sub
+
+        const user = await prisma.user.upsert({
+            where: {
+                uuid: userUUID
+            },
+            update: {},
+            create: {
+                uuid: userUUID
+            }
+        })
+
+        req.userId = user.id
+        next()
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+}
