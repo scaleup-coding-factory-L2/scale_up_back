5a7926ad3712eb6bd16db3069a3afb4b6435910b
alex <alexandre.souvorov@edu.esiee-it.fr>
2024-03-01 10:54:04 +0100

    feature: api for needs request

---
+++ src/controllers/NeedController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express'
+import { prisma } from '../index'
+
+export const getNeeds = async (req: Request, res: Response) => {
+    let needs = await prisma.need.findMany().catch((error) => {
+        console.error('Error fetching needs:', error)
+        res.status(500).json({ error: 'Error fetching needs' })
+    })
+    res.status(200).json(needs)
+}
+
+export const getNeedsByYear = async (req: Request, res: Response) => {
+    let year = req.params.year
+    let startYears = year.split('-')
+    let startYear = startYears[0]
+    let endYear = startYears[1]
+    let needs = await prisma.need.findMany({
+        where: {
+            AND: [
+                {
+                    startSchoolYear: {
+                        equals: parseInt(startYear)
+                    }
+                },
+                {
+                    endSchoolYear: {
+                        equals: parseInt(endYear)
+                    }
+                }
+            ]
+        }
+    }).catch((error) => {
+        console.error('Error fetching needs:', error)
+        res.status(500).json({ error: 'Error fetching needs' })
+    })
+    res.status(200).json(needs)
+}
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import helloRoutes from './routes/helloRoutes'
+import needsRoutes from './routes/needsRoutes'
 import morgan from 'morgan'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+
+export const prisma = new PrismaClient()
+
 
 app.use(helmet())
 app.use(cors())
@@ -12,5 +17,7 @@ app.use(express.json())
 app.use(morgan('tiny'))
 
 app.use('/api', helloRoutes)
+app.use('/api/needs', needsRoutes)
+
 
 export { app }
