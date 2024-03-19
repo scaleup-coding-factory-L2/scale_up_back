6e26d7e5882372e5b056d286631b32bf4df03088
Turbotard <turbotard9@gmail.com>
2024-03-19 14:13:33 +0100

    Delete unused QuotationMiddelwares.ts and add selectAllQuotations middleware

--- src/middlewares/QuotationMiddelwares.ts
+++ src/middlewares/QuotationMiddelwares.ts
@@ -33,9 +33,19 @@ export const updateQuotationStatus = async (req: Request, res: Response) => {
                 status,
             },
         });
-        res.status(200).json({ message: 'Quotation status updated successfully', quotation: updatedQuotation });
+        res.status(200).json({ message: 'Quotation status updated successfully' });
     } catch (error) {
         console.error('Error updating quotation status');
         res.status(500).json({ message: 'Failed to update quotation status' });
+    }
+}
+
+export const selectAllQuotations = async (req: Request, res: Response) => {
+    try {
+        const quotations = await prisma.quotation.findMany();
+        res.status(200).json({ message: 'Quotations fetched successfully', quotations });
+    } catch (error) {
+        console.error('Error fetching quotations:');
+        res.status(500).json({ message: 'Failed to fetch quotations' });
     }
 }
