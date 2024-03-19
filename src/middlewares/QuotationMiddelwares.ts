4865457ded760e3be840ccd08548f687058e7a1d
Turbotard <turbotard9@gmail.com>
2024-03-19 14:17:22 +0100

    Add quotation deletion and update functionality

--- src/middlewares/QuotationMiddelwares.ts
+++ src/middlewares/QuotationMiddelwares.ts
@@ -43,9 +43,46 @@ export const updateQuotationStatus = async (req: Request, res: Response) => {
 export const selectAllQuotations = async (req: Request, res: Response) => {
     try {
         const quotations = await prisma.quotation.findMany();
-        res.status(200).json({ message: 'Quotations fetched successfully', quotations });
+        res.status(200).json({ message: 'Quotations fetched successfully' });
     } catch (error) {
         console.error('Error fetching quotations:');
         res.status(500).json({ message: 'Failed to fetch quotations' });
+    }
+}
+
+export const deleteQuotation = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        await prisma.quotation.delete({
+            where: {
+                id: parseInt(id),
+            },
+        });
+        res.status(200).json({ message: 'Quotation deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting quotation:');
+        res.status(500).json({ message: 'Failed to delete quotation' });
+    }
+}
+
+export const updateQuotation = async (req: Request, res: Response) => {
+    try {
+        const { id, price, status, bills, purchaseOrder, date } = req.body;
+        const updatedQuotation = await prisma.quotation.update({
+            where: {
+                id: parseInt(id),
+            },
+            data: {
+                price,
+                status,
+                bills,
+                purchaseOrder,
+                date,
+            },
+        });
+        res.status(200).json({ message: 'Quotation updated successfully' });
+    } catch (error) {
+        console.error('Error updating quotation');
+        res.status(500).json({ message: 'Failed to update quotation' });
     }
 }
