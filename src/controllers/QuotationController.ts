47779a32d63059eb071156628c284e177ee25f4e
Turbotard <turbotard9@gmail.com>
2024-03-19 14:18:57 +0100

    Add new quotation CRUD endpoints

--- src/controllers/QuotationController.ts
+++ src/controllers/QuotationController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import {
     selectQuotation as selectQuotationMiddleware,
-    updateQuotationStatus as updateQuotationStatusMiddleware
+    updateQuotationStatus as updateQuotationStatusMiddleware,
+    selectAllQuotations as selectAllQuotationsMiddleware,
+    deleteQuotation as deleteQuotationMiddleware,
+    updateQuotation as updateQuotationMiddleware
 } from '../middlewares/QuotationMiddelwares';
 
 export const selectQuotation = async (req: Request, res: Response) => {
@@ -11,3 +14,16 @@ export const selectQuotation = async (req: Request, res: Response) => {
 export const updateQuotationStatus = async (req: Request, res: Response) => {
   return updateQuotationStatusMiddleware(req, res);
 };
+
+export const selectAllQuotations = async (req: Request, res: Response) => {
+  return selectAllQuotationsMiddleware(req, res);
+};
+
+export const deleteQuotation = async (req: Request, res: Response) => {
+  return deleteQuotationMiddleware(req, res);
+};
+
+export const updateQuotation = async (req: Request, res: Response) => {
+  return updateQuotationMiddleware(req, res);
+};
+
--- src/routes/quotationRoutes.ts
+++ src/routes/quotationRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 
-import { selectQuotation, updateQuotationStatus } from '../controllers/QuotationController'
+import { selectQuotation, updateQuotationStatus, updateQuotation, deleteQuotation, selectAllQuotations } from '../controllers/QuotationController'
 
 const router = Router()
 
 router.get('/selectQuotation/:id', selectQuotation)
 router.put('/updateQuotationStatus/:id', updateQuotationStatus)
+router.put('/updateQuotation/:id', updateQuotation)
+router.delete('/deleteQuotation/:id', deleteQuotation)
+router.get('/selectAllQuotations', selectAllQuotations)
 
 export default router
